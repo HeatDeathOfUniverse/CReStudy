903894cb54b4fe3f5ef6f60fd5dcab81b69aacdb
HeatDeathOfUniverse <53603499+HeatDeathOfUniverse@users.noreply.github.com>
2024-01-09 22:45:02 +0800

    新建调试功能示例代码

--- Chapter1/02hello_world.c
+++ Chapter1/02hello_world.c
@@ -2,8 +2,5 @@
 
 int main() {
     printf("Hello, World!\n");
-    for (int i = 0; i < 5; ++i) {
-        printf("i=%d\n", i );
-    }
     return 0;
 }
---
+++ Chapter1/03.debug_c_program.c
@@ -0,0 +1,11 @@
+//
+// Created by NegentropyHost on 2024/1/9.
+//
+#include <stdio.h>
+int main() {
+    printf("Hello, World!\n");
+    for (int i = 0; i < 5; ++i) {
+        printf("i=%d\n", i );
+    }
+    return 0;
+}
