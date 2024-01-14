b3049aea58048b073184165d7e631a216243f482
HeatDeathOfUniverse <53603499+HeatDeathOfUniverse@users.noreply.github.com>
2024-01-14 19:36:05 +0800

    Create 08.loop.c

---
+++ Chapter3/08.loop.c
@@ -0,0 +1,58 @@
+//
+// Created by NegentropyHost on 2024/1/14.
+//
+#include <stdio.h>
+int main(){
+  /*
+   * while (<condition<){
+   *    statement
+   * }
+   */
+
+  /*
+   * do {
+   *   statement
+   * } while (<condition>);
+   */
+#define ADD '+'
+#define SUB '-'
+#define MULTIPLY '*'
+#define DIVIDE '/'
+#define REM '%'
+  int left;
+  int right;
+  char operator;
+
+  char command = 'y';
+
+  while(command != 'q'){
+
+    printf("Please input expression:\n");
+    scanf("%d %c %d", &left, &operator, &right);
+    int result;
+    // switch
+    switch (operator) {
+      case ADD:
+        result = left + right;
+        break;
+      case SUB:
+        result = left - right;
+        break;
+      case MULTIPLY:
+        result = left * right;
+        break;
+      case DIVIDE:
+        result = left / right;
+        break;
+      default:
+        printf("Unsupported operation: %c\n", operator);
+        return 1;
+
+    }
+    printf("Result:%d\n", result);
+    printf("Again? Type 'q' for quit:\n");
+    getchar();// 用于获取输入表达式时最后确认的换行（回车）
+    command = getchar();
+  }
+  return 0;
+}
