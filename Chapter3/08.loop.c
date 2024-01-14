c4ae2d5f46c37f72a5922a24ece67e7dafe440e1
HeatDeathOfUniverse <53603499+HeatDeathOfUniverse@users.noreply.github.com>
2024-01-14 20:02:04 +0800

    Update 08.loop.c
    for 循环，continue break；goto

--- Chapter3/08.loop.c
+++ Chapter3/08.loop.c
@@ -2,7 +2,7 @@
 // Created by NegentropyHost on 2024/1/14.
 //
 #include <stdio.h>
-int main(){
+int main() {
   /*
    * while (<condition<){
    *    statement
@@ -14,45 +14,88 @@ int main(){
    *   statement
    * } while (<condition>);
    */
-#define ADD '+'
-#define SUB '-'
-#define MULTIPLY '*'
-#define DIVIDE '/'
-#define REM '%'
-  int left;
-  int right;
-  char operator;
+//#define ADD '+'
+//#define SUB '-'
+//#define MULTIPLY '*'
+//#define DIVIDE '/'
+//#define REM '%'
+//  int left;
+//  int right;
+//  char operator;
+//
+//  char command;
+//
+//  do{
+//
+//    printf("Please input expression:\n");
+//    scanf("%d %c %d", &left, &operator, &right);
+//    int result;
+//    // switch
+//    switch (operator) {
+//      case ADD:
+//        result = left + right;
+//        break;
+//      case SUB:
+//        result = left - right;
+//        break;
+//      case MULTIPLY:
+//        result = left * right;
+//        break;
+//      case DIVIDE:
+//        result = left / right;
+//        break;
+//      default:
+//        printf("Unsupported operation: %c\n", operator);
+//        return 1;
+//
+//    }
+//    printf("Result:%d\n", result);
+//    printf("Again? Type 'q' for quit:\n");
+//    getchar();// 用于获取输入表达式时最后确认的换行（回车）
+//    command = getchar();
+//  }while(command != 'q');
 
-  char command;
+/*
+ * for (<initialization>; <condition>; <state>){
+ *   statement
+ * }
+ * < C99
+ * initialization 不能有变量定义
+ */
 
-  do{
+//// < C99 num在外面定义
+//  int num;
+//  int sum = 0;
 
-    printf("Please input expression:\n");
-    scanf("%d %c %d", &left, &operator, &right);
-    int result;
-    // switch
-    switch (operator) {
-      case ADD:
-        result = left + right;
-        break;
-      case SUB:
-        result = left - right;
-        break;
-      case MULTIPLY:
-        result = left * right;
-        break;
-      case DIVIDE:
-        result = left / right;
-        break;
-      default:
-        printf("Unsupported operation: %c\n", operator);
-        return 1;
+//while (num < 100){
+//  sum += num;
+//  num++;
+//}
+  int sum = 0;
+  for (int num = 1; num <= 100; ++num) {
+    sum += num;
+  }
+  printf("sum of [0,100]: %d\n", sum);
 
-    }
-    printf("Result:%d\n", result);
-    printf("Again? Type 'q' for quit:\n");
-    getchar();// 用于获取输入表达式时最后确认的换行（回车）
-    command = getchar();
-  }while(command != 'q');
+  int sumij = 0;
+  for (int i = 0, j = 0; i <= 100 && j <= 100; ++i, ++j) {
+    sumij += i * (i + j);
+  }
+  printf("sumij is :%d\n", sumij);
+
+  //continue break
+  for (int i = 0; i < 10; ++i) {
+    if (i == 2) continue;
+    if (i == 8)break;
+    printf("i:%d\n", i);
+  }
+
+  // goto
+  int i = 0;
+  begin:
+  printf("%d\n",i);
+  if (i++ < 5){
+    goto begin;
+  }
   return 0;
 }
