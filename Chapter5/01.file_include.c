0cfc17d49bd9059f06ec965c0f0d42922745a728
HeatDeathOfUniverse <53603499+HeatDeathOfUniverse@users.noreply.github.com>
2024-01-17 20:16:51 +0800

    Create 01.file_include.c

---
+++ Chapter5/01.file_include.c
@@ -0,0 +1,12 @@
+//
+// Created by NegentropyHost on 2024/1/17.
+//
+#include <stdio.h>
+
+//int __cdecl puts(
+//    char const* _Buffer
+//);
+int main() {
+  puts("Hello, World!");
+  return 0;
+}
