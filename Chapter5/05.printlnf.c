16201ac0d90536228449f1f6188c092096341dd1
HeatDeathOfUniverse <53603499+HeatDeathOfUniverse@users.noreply.github.com>
2024-01-19 21:02:01 +0800

    Update 05.printlnf.c
    +新增 不输入变长参数也能打印、项目信息
    +新增 传入参数和表达式打印其值

--- Chapter5/05.printlnf.c
+++ Chapter5/05.printlnf.c
@@ -11,13 +11,31 @@ void Printlnf(const char *format, ...){
   printf("\n");
   va_end(args);
 }
-
+/*
+ * 元数据宏
+ * __FILE__
+ * __LINE__
+ * __FUNCTION__
+ */
 // 2. 宏实现printlnf
-#define PRINTLNF(format, ...) printf(format"\n", __VA_ARGS__)
+//#define PRINTLNF(format, ...) printf(format"\n", __VA_ARGS__)// 2. 宏实现printlnf
+// 2.1 新增可以不输入变长参数 +两个##即可
+//#define PRINTLNF(format, ...) printf(format"\n", ##__VA_ARGS__)
+//2.2 新增打印文件路径以及行号和方法名
+#define PRINTLNF(format, ...) printf("("__FILE__":%d) %s :"  format"\n", __LINE__, __FUNCTION__ ,##__VA_ARGS__)
 
+// 3. 新增传入参数和表达式打印其值
+#define PRINTLNF_INT(value) printf(#value " %d\n", value);
 int main(){
   int value = 2;
   Printlnf("Hello World! %d", value);
   PRINTLNF("HELLO WORLD! %d", value);
+
+  // 2.1 测试不输出变长参数
+  PRINTLNF("HELLO WORLD!!!");
+
+  //3 测试打印值和表达式
+  PRINTLNF_INT(value);
+  PRINTLNF_INT(4 + 5);
   return 0;
 }
